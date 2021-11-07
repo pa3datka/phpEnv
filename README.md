d3f0d01f1521e22cbbae1424d40ba410bb93d63d
Ðpa3datka <pa3datka@gmail.com>
2021-11-07 15:03:35 +0300

    init lib

---
+++ README.md
@@ -0,0 +1,2 @@
+A simple library for reading the .env file. Create a file .env in the root of the project. 
+Use: `Env::env(string $value, string $default): string`
---
+++ src/Env.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace Pa3datka;
+
+
+final class Env
+{
+    private static $instance;
+
+    private array $environment;
+
+
+    private static function getInstance(): Env
+    {
+        if (!self::$instance) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+    }
+
+    public static function env(string $value, string $default = ''): string
+    {
+        $envClass = self::getInstance();
+
+        return $envClass->getValue($value, $default);
+    }
+
+    private function getValue(string $value, string $default): string
+    {
+       !isset($this->environment) && $this->getEnvironment();
+
+       return $this->environment[$value] ?? $default;
+    }
+
+    private function getEnvironment(): void
+    {
+        $env = $this->getText();
+        $this->getMath($env);
+    }
+
+    private function getText(): string
+    {
+        $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '.env';
+
+        return file_get_contents($path);
+    }
+
+    private function getMath($env): void
+    {
+        $this->environment = [];
+
+        if (preg_match_all('#([A-Z\d_]*)=(.*)\n#', $env, $math)) {
+            $cntArr = count($math[1]);
+            for ($i = 0; $i < $cntArr; $i++) {
+                $this->environment[$math[1][$i]] = $math[2][$i];
+            }
+        }
+    }
+
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize a singleton.");
+    }
+
+    protected function __clone() { }
+}
